71576e1922ce3fb242767bd639c3f2ca3056f85b
Ethan Lanting <ethanlanting@gmail.com>
2024-11-02 14:21:57 -0500

    changes to landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const pressStart2P = Press_Start_2P({
+  weight: "400",
+  preload: true,
+  display: "swap",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${pressStart2P.className} antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,46 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World
+    <div className="relative max-h-screen overflow-hidden">
+      <div className="absolute left-0 top-36 z-0 overflow-x-hidden">
+        <div className="relative flex w-full flex-nowrap">
+          <div className="animate-infinite-scroll-x flex h-screen w-screen items-center justify-center md:justify-start">
+            <img
+              src="./images/background.png"
+              alt="background"
+              className="size-full object-cover"
+            />
+          </div>
+          <div className="animate-infinite-scroll-x flex h-screen w-screen items-center justify-center md:justify-start">
+            <img
+              src="./images/background.png"
+              alt="background"
+              className="size-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="relative bottom-14 z-50 flex min-h-screen flex-col items-center justify-center p-5">
+        <div className="mb-10 flex flex-col gap-y-2">
+          <h1 className="retro-text text-center text-4xl text-[#85B6F2]">
+            Adventurely
+          </h1>
+          <p className="retro-text text-center text-sm">Get Your Work Done.</p>
+        </div>
+        <div className="flex flex-col gap-y-4">
+          <Button className="" variant={"retro"}>
+            <div className="relative">
+              <i className="nes-icon google"></i>
+            </div>
+            <div className="ml-7">Sign In with Google</div>
+          </Button>
+          <Button className="text-sm" variant={"ghost"}>
+            Sign In as Guest
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
