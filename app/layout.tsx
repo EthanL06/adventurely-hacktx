90e419c025e09d0a9d0427c6ebffd189b75ffe6e
Tomin <tchazhikat@gmail.com>
2024-11-02 16:25:25 -0500

    more misc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
+import Background from "@/components/ui/background";
 
 const pressStart2P = Press_Start_2P({
   weight: "400",
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${pressStart2P.className} antialiased`}>
+      <body
+        className={`${pressStart2P.className} relative bg-[#FFE9D9] antialiased`}
+      >
+        <Background />
         {children}
       </body>
     </html>
